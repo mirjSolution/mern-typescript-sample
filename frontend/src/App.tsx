22e3d7f9368bbd66318ce05bfd49b3e9d8b25700
mirjSolution <ricojohndatoon23@gmail.com>
2023-01-20 15:32:50 -0500

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { Note } from './models/note';
+
+function App() {
+  const [notes, setNotes] = React.useState<Note[]>([]);
+
+  React.useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch('/api/notes', {
+          method: 'GET',
+        });
+
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+
+    loadNotes();
+  }, []);
+
+  return <div className='App'>{JSON.stringify(notes)}</div>;
+}
+
+export default App;
---
+++ frontend/src/models/note.ts
@@ -0,0 +1,7 @@
+export interface Note {
+  _id: string;
+  title: string;
+  text?: string;
+  createAt: string;
+  updatedAt: string;
+}
